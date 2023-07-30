d4b8dd13e973d1da688e1b0c5b4bd841d7de37c8
Sanket Patel <sanketpatel2108@gmail.com>
2023-07-30 16:06:51 +0530

    Theme color update - #000

--- public/assets/js/config.js
+++ public/assets/js/config.js
@@ -10,7 +10,7 @@
 // JS global variables
 let config = {
   colors: {
-    primary: '#7367f0',
+    primary: '#000',
     secondary: '#a8aaae',
     success: '#28c76f',
     info: '#00cfe8',
@@ -27,7 +27,7 @@ let config = {
     borderColor: '#dbdade'
   },
   colors_label: {
-    primary: '#7367f029',
+    primary: '#00029',
     secondary: '#a8aaae29',
     success: '#28c76f29',
     info: '#00cfe829',
