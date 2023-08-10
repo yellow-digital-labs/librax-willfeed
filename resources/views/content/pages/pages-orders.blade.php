e7c446674c6f9ce045ba4eeef12d2bd8d36be415
Sanket Patel <sanketpatel2108@gmail.com>
2023-08-10 23:33:54 +0530

    Seller Order list

--- resources/views/content/pages/pages-orders.blade.php
+++ resources/views/content/pages/pages-orders.blade.php
@@ -11,10 +11,6 @@ $configData = Helper::appClasses();
 <link rel="stylesheet" href="{{asset('assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css')}}">
 <link rel="stylesheet" href="{{asset('assets/vendor/libs/flatpickr/flatpickr.css')}}" />
 @endsection
-@section('page-style')
-<!-- Custom css -->
-<link rel="stylesheet" href="{{asset('assets/css/custom.css')}}">
-@endsection
 
 @section('vendor-script')
 <script src="{{asset('assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js')}}"></script>
@@ -25,80 +21,98 @@ $configData = Helper::appClasses();
 @endsection
 
 @section('page-script')
-<script src="{{asset('assets/js/custom/simple-search-datatable.js')}}"></script>
+<!-- <script src="{{asset('assets/js/tables-datatables-advanced.js')}}"></script> -->
+<script src="{{asset('assets/js/approved-customer-datatables.js')}}"></script>
+@endsection
+
+@section('page-style')
+<!-- Custom css -->
+<link rel="stylesheet" href="{{asset('assets/css/custom.css')}}">
 @endsection
 
 @section('content')
 
-<h4>Orders</h4>
+<h1 class="h3 text-black mb-4">Ordini</h1>
+
+<div class="card mb-4">
+    <div class="card-widget-separator-wrapper">
+        <div class="card-body card-widget-separator">
+            <div class="row gy-4 gy-sm-1">
+                <div class="col-sm-6 col-lg-3">
+                    <div class="d-flex justify-content-between align-items-start card-widget-1 border-end pb-3 pb-sm-0">
+                        <div>
+                            <h6 class="mb-2 fw-normal">Ordini totali</h6>
+                            <h4 class="mb-2">5,000</h4>
+                            <p class="mb-0"><span class="badge bg-label-success fw-normal">100.00%</span></p>
+                        </div>
+                        <span class="avatar avatar--prodotti me-sm-4">
+                            <span class="avatar-initial bg-label-secondary rounded"><i class="ti-sm ti ti-smart-home text-body"></i></span>
+                        </span>
+                    </div>
+                    <hr class="d-none d-sm-block d-lg-none me-4">
+                </div>
+                <div class="col-sm-6 col-lg-3">
+                    <div class="d-flex justify-content-between align-items-start card-widget-2 border-end pb-3 pb-sm-0">
+                        <div>
+                            <h6 class="mb-2 fw-normal">Ordini in Euro</h6>
+                            <h4 class="mb-2">€4,00,000</h4>
+                            <p class="mb-0"><span class="badge bg-label-success fw-normal">80.00%</span></p>
+                        </div>
+                        <span class="avatar avatar--prodotti me-sm-4">
+                            <span class="avatar-initial bg-label-secondary rounded"><i class="ti-sm ti ti-smart-home text-body"></i></span>
+                        </span>
+                    </div>
+                    <hr class="d-none d-sm-block d-lg-none">
+                </div>
+                <div class="col-sm-6 col-lg-3">
+                    <div class="d-flex justify-content-between align-items-start border-end pb-3 pb-sm-0 card-widget-3">
+                        <div>
+                            <h6 class="mb-2 fw-normal">Ordini Cancellati</h6>
+                            <h4 class="mb-2">1,000</h4>
+                            <p class="mb-0"><span class="badge bg-label-danger fw-normal">20.00%</span></p>
+                        </div>
+                        <span class="avatar avatar--prodotti p-2 me-sm-4">
+                            <span class="avatar-initial bg-label-secondary rounded"><i class="ti-sm ti ti-smart-home text-body"></i></span>
+                        </span>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3">
+                    <div class="d-flex justify-content-between align-items-start">
+                        <div>
+                            <h6 class="mb-2 fw-normal">Ordini completati</h6>
+                            <h4 class="mb-2">4,000</h4>
+                            <p class="mb-0"><span class="badge bg-label-danger">20.00%</span></p>
+                        </div>
+                        <span class="avatar avatar--prodotti p-2">
+                            <span class="avatar-initial bg-label-secondary rounded"><i class="ti-sm ti ti-smart-home text-body"></i></span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <div class="card">
-    <!-- <h5 class="card-header">Light Table head</h5> -->
+    <div class="card-header d-flex justify-content-between border-bottom">
+        <div class="card-title mb-0">
+            <h5 class="mb-0 text-black">Lista ordini</h5>
+        </div>
+    </div>
     <div class="card-datatable text-nowrap">
-        <table class="dt-column-search table has-actions-td">
+        <table class="dt-column-search table">
             <thead>
                 <tr>
-                    <th>Name</th>
-                    <th>Lorem Field</th>
-                    <th>Lorem Field</th>
-                    <th>Lorem Field</th>
-                    <th>Lorem Field</th>
-                    <th class="w-px-100">Actions</th>
+                    <th class="js-add-search">Cliente</th>
+                    <th class="js-add-search">Prodotto</th>
+                    <th>quantità</th>
+                    <th>data</th>
+                    <th>status</th>
+                    <th>Azione</th>
                 </tr>
             </thead>
-            <tbody class="table-border-bottom-0">
-                <tr>
-                    <td>Lorem come here 1</td>
-                    <td>Lorem come here</td>
-                    <td>Lorem come here</td>
-                    <td>Lorem come here</td>
-                    <td>Lorem come here</td>
-                    <td>
-                        <a href="javascript:;" class="btn btn-sm btn-icon item-edit"><i class="text-primary ti ti-pencil"></i></a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Lorem come here 2</td>
-                    <td>Lorem come here</td>
-                    <td>Lorem come here</td>
-                    <td>Lorem come here</td>
-                    <td>Lorem come here</td>
-                    <td>
-                        <a href="javascript:;" class="btn btn-sm btn-icon item-edit"><i class="text-primary ti ti-pencil"></i></a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Lorem come here 3</td>
-                    <td>Lorem come here</td>
-                    <td>Lorem come here</td>
-                    <td>Lorem come here</td>
-                    <td>Lorem come here</td>
-                    <td>
-                        <a href="javascript:;" class="btn btn-sm btn-icon item-edit"><i class="text-primary ti ti-pencil"></i></a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Lorem come here 4</td>
-                    <td>Lorem come here</td>
-                    <td>Lorem come here</td>
-                    <td>Lorem come here</td>
-                    <td>Lorem come here</td>
-                    <td>
-                        <a href="javascript:;" class="btn btn-sm btn-icon item-edit"><i class="text-primary ti ti-pencil"></i></a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Lorem come here 5</td>
-                    <td>Lorem come here</td>
-                    <td>Lorem come here</td>
-                    <td>Lorem come here</td>
-                    <td>Lorem come here</td>
-                    <td>
-                        <a href="javascript:;" class="btn btn-sm btn-icon item-edit"><i class="text-primary ti ti-pencil"></i></a>
-                    </td>
-                </tr>
-            </tbody>
         </table>
     </div>
 </div>
+
 @endsection
