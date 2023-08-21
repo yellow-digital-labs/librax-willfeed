32f03572d4c39efc82971290e24c9e2d02fc965f
Yellow Digital Labs <yellow.digital.labs@gmail.com>
2023-08-21 17:11:14 +0530

    update migrations
    update migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,15 +17,21 @@ return new class extends Migration
             $table->string("accountTypeName")->nullable();
             $table->string('name')->nullable();
             $table->string('email')->unique();
+            $table->string('verification_token')->nullable();
+            $table->datetime('varification_valid_till')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->integer('subscription_id')->nullable();
+            $table->float('subscription_amount', 8, 2)->default(0);
+            $table->string('subscription_name')->default('');
             $table->datetime('exp_datetime')->index();
             $table->integer('account_type_id')->index();
             $table->integer('account_name');
             $table->string('profile_photo_path', 2048)->nullable();
+            $table->enum('profile_completed', ['Yes', 'No'])->default('No')->index();
+            $table->enum('approved_by_admin', ['Yes', 'No'])->default('No')->index();
             $table->timestamps();
         });
     }
