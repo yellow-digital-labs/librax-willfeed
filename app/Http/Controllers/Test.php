061ce59359918a333bfe1a21c3e9cca3fc666247
Yellow Digital Labs <yellow.digital.labs@gmail.com>
2023-10-05 14:49:55 +0530

    test time
    test time

--- app/Http/Controllers/Test.php
+++ app/Http/Controllers/Test.php
@@ -9,6 +9,7 @@ use App\Mail\UserVerification;
 class Test extends Controller
 {
     public function index(){
+        echo date("Y-m-d H:i:s");die;
         Mail::to('tejas.ambalia1994@gmail.com')->send(new UserVerification([
             "accountTypeName" => 'Seller',
             "verificationUrl" => 'google.com'
