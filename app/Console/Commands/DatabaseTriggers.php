3c952462c7adb20d6b842e514d3bc443364c0603
Yellow Digital Labs <yellow.digital.labs@gmail.com>
2023-10-16 17:59:56 +0530

    trigger text updated
    trigger text updated

--- app/Console/Commands/DatabaseTriggers.php
+++ app/Console/Commands/DatabaseTriggers.php
@@ -141,12 +141,12 @@ class DatabaseTriggers extends Command
         DB::unprepared('DROP TRIGGER IF EXISTS `orders_before_update`');
         DB::unprepared('CREATE TRIGGER orders_before_update BEFORE UPDATE ON `orders` FOR EACH ROW
                 BEGIN
-                    IF NEW.order_status_id <> OLD.order_status_id THEN
+                    -- IF NEW.order_status_id <> OLD.order_status_id THEN
                         (SELECT name INTO @order_status FROM order_statuses WHERE id=NEW.order_status_id);
                         SET NEW.order_status = @order_status;
 
-                        INSERT INTO order_activity_histories SET order_id=NEW.id, status_title=CONCAT("Order ", @order_status), status_description=CONCAT("Order status successfully updated to ", @order_status), status_updated_at=NEW.updated_at;
-                    END IF;
+                        INSERT INTO order_activity_histories SET order_id=NEW.id, status_title=CONCAT("Ordine ", @order_status), status_description=CONCAT("Ordine stato aggiornato con successo a ", @order_status), status_updated_at=NEW.updated_at;
+                    -- END IF;
 
                     IF NEW.total_paid_amount <> OLD.total_paid_amount OR NEW.total_payable_amount <> OLD.total_payable_amount THEN
                         SET NEW.total_pending_amount = NEW.total_payable_amount - NEW.total_paid_amount;
@@ -160,9 +160,9 @@ class DatabaseTriggers extends Command
 
                     IF NEW.payment_status <> OLD.payment_status THEN
                         IF NEW.payment_status = "paid" THEN
-                            INSERT INTO order_activity_histories SET order_id=NEW.id, status_title="Paid", status_description="Order paid successfully", status_updated_at=NEW.updated_at;
+                            INSERT INTO order_activity_histories SET order_id=NEW.id, status_title="Paid", status_description="Ordine pagato con successo", status_updated_at=NEW.updated_at;
                         ELSE
-                            INSERT INTO order_activity_histories SET order_id=NEW.id, status_title="Unpaid", status_description="Order unpaid successfully", status_updated_at=NEW.updated_at;
+                            INSERT INTO order_activity_histories SET order_id=NEW.id, status_title="Unpaid", status_description="Ordine non pagato con successo", status_updated_at=NEW.updated_at;
                         END IF;
                     END IF;
                 END');
