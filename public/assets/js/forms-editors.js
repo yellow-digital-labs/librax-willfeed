fd62576ce6044a18153c7d3824734057f9248d97
Yellow Digital Labs <yellow.digital.labs@gmail.com>
2023-10-28 11:41:28 +0530

    update editor change
    update editor change

--- public/assets/js/forms-editors.js
+++ public/assets/js/forms-editors.js
@@ -7,18 +7,18 @@
 (function () {
   // Snow Theme
   // --------------------------------------------------------------------
-  // const snowEditor = new Quill('#snow-editor', {
-  //   bounds: '#snow-editor',
-  //   modules: {
-  //     formula: true,
-  //     toolbar: '#snow-toolbar'
-  //   },
-  //   theme: 'snow'
-  // });
+  const snowEditor = new Quill('#snow-editor', {
+    bounds: '#snow-editor',
+    modules: {
+      formula: true,
+      toolbar: '#snow-toolbar'
+    },
+    theme: 'snow'
+  });
 
-  // snowEditor.pasteHTML(description);
+  snowEditor.pasteHTML(description);
 
-  // snowEditor.on('text-change', () => {
-  //     $("#text_quill").val(snowEditor.root.innerHTML);
-  // });
+  snowEditor.on('text-change', () => {
+      $("#text_quill").val(snowEditor.root.innerHTML);
+  });
 })();
