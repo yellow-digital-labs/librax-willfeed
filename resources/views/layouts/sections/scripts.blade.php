90217070d9390274fc82e432aeaa1e9993a1a507
Yellow Digital Labs <yellow.digital.labs@gmail.com>
2024-01-08 15:13:26 +0530

    translate and socila media update
    translate and socila media update

--- resources/views/layouts/sections/scripts.blade.php
+++ resources/views/layouts/sections/scripts.blade.php
@@ -23,3 +23,46 @@
 @stack('modals')
 @livewireScripts
 <script src="{{asset(mix('js/alpine.js'))}}"></script>
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+<script type="text/javascript">
+    function googleTranslateElementInit() {
+      new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+    }
+
+    function getCookie(name) {
+        var dc = document.cookie;
+        var prefix = name + "=";
+        var begin = dc.indexOf("; " + prefix);
+        if (begin == -1) {
+            begin = dc.indexOf(prefix);
+            if (begin != 0) return null;
+        }
+        else
+        {
+            begin += 2;
+            var end = document.cookie.indexOf(";", begin);
+            if (end == -1) {
+            end = dc.length;
+            }
+        }
+        // because unescape has been deprecated, replaced with decodeURI
+        //return unescape(dc.substring(begin + prefix.length, end));
+        return decodeURI(dc.substring(begin + prefix.length, end));
+    }
+
+    let transAvail = getCookie("googtrans");
+    if(transAvail){
+
+    } else {
+        document.cookie='googtrans=/en/it;';
+        location.reload();
+    }
+    window.onload = function(){
+        document.getElementsByTagName('body')[0].removeAttribute("style");
+    }
+</script>
+<style>
+    .skiptranslate{
+        display: none;
+    }
+</style>
