a1b2c2197699d5199656b0051d1bf939f09faf78
vimal112 <vimal1122001@gmail.com>
2024-03-28 20:59:06 +0530

    add ip address column & stored on accept and reject cookie

--- packages/yellowdigitallabs/laravel-cookie-consent/src/Http/Controllers/AcceptEssentialsController.php
+++ packages/yellowdigitallabs/laravel-cookie-consent/src/Http/Controllers/AcceptEssentialsController.php
@@ -10,23 +10,27 @@ class AcceptEssentialsController
 {
     public function __invoke(Request $request, CookiesManager $cookies)
     {
-        if($request->user()){
+        if ($request->user()) {
             $user_type = $request->user()->accountTypeName . ' | ' . $request->user()->name;
 
-            CookieModel::updateOrCreate([
-                'user_id' => $request->user()->id,
-            ],
-            [
-                'user_id' => $request->user()->id,
-                'user_name' => $user_type,
-                'consents' => 'Rejected All',
-                'created_at' => date('Y-m-d H:i:s')
-            ]);
+            CookieModel::updateOrCreate(
+                [
+                    'user_id' => $request->user()->id,
+                ],
+                [
+                    'user_id' => $request->user()->id,
+                    'user_name' => $user_type,
+                    'ip_address' =>  $request->ip(),
+                    'consents' => 'Rejected All',
+                    'created_at' => date('Y-m-d H:i:s')
+                ]
+            );
         } else {
             $user_type = 'Guest';
 
             CookieModel::insert([
                 'user_name' => $user_type,
+                'ip_address' =>  $request->ip(),
                 'consents' => 'Rejected All',
                 'created_at' => date('Y-m-d H:i:s')
             ]);
