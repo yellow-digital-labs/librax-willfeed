c52298434ed0602d9e1ec694d1c52970773e4321
vimal112 <vimal1122001@gmail.com>
2024-10-14 23:02:49 +0530

    redirect to login when page expired

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\AuthenticationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -44,5 +45,12 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception){
+        if ($exception instanceof AuthenticationException) {
+            return redirect('/login');
+        }
+        return parent::render($request, $exception);
     }
 }
